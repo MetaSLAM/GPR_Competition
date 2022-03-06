f4baabc7a5926ead1b5359481037cb94336d19ea
maxtomCMU <pyin2@andrew.cmu.edu>
2022-03-06 16:19:01 -0500

    update test

---
+++ tests/test_cmu.py
@@ -0,0 +1,22 @@
+'''
+Filename: /home/maxtom/codespace/GPR_Competition/tests/test_cmu.py
+Path: /home/maxtom/codespace/GPR_Competition/tests
+Created Date: Friday, March 4th 2022, 5:08:28 pm
+Author: maxtom
+
+Copyright (c) 2022 Your Company
+'''
+
+import gpr
+import numpy as np
+
+
+
+
+#* Test Data Loader
+
+#* Test Raw Data Visualization
+
+#* Test Feature Extraction
+
+#* Test Place Recognition Results
